10f125fb7f692cba4a29b8a4642efcb1ae406798
ABHINAV TIWARI <TIWARIABHINAV273@GMAIL.COM>
2025-04-25 02:26:48 +0530

    Add files via upload

---
+++ def friends_in_trouble(j_angry, s_angry).py
@@ -0,0 +1,7 @@
+def friends_in_trouble(j_angry, s_angry):
+    if (j_angry == True and s_angry == True):
+        return True   
+    if (j_angry is False and s_angry is False):
+        return True
+    return False
+    
