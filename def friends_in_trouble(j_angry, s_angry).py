9fbd75047271df6386141272326324a5b32fca16
ABHINAV TIWARI <TIWARIABHINAV273@GMAIL.COM>
2025-04-25 02:30:23 +0530

    Update def friends_in_trouble(j_angry, s_angry).py

--- def friends_in_trouble(j_angry, s_angry).py
+++ def friends_in_trouble(j_angry, s_angry).py
@@ -1,7 +1,6 @@
 def friends_in_trouble(j_angry, s_angry):
     if (j_angry == True and s_angry == True):
-        return True   
+        return True
     if (j_angry is False and s_angry is False):
         return True
     return False
-    
